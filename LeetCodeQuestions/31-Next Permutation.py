31aea6551fa07ea2d8dd359a83c2e813ded62ef1
Aman Mishra <72151791+AmanMishra04@users.noreply.github.com>
2023-10-02 18:22:30 +0530

    Create 31-Next Permutation.py
    LeetCode 31 - Next Permutation solution

---
+++ LeetCodeQuestions/31-Next Permutation.py
@@ -0,0 +1,38 @@
+#Problem Statement:
+#Implement the next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
+#If such an arrangement is not possible, it must be rearranged to the lowest possible order (i.e., sorted in ascending order).
+#The replacement must be in place and use only constant extra memory.
+
+def reverse(nums, i, j):
+    while i < j:
+        nums[i], nums[j] = nums[j], nums[i]
+        i += 1
+        j -= 1
+
+
+def nextPermutation(nums: List[int]):
+    # Length of the array
+    n = len(nums)
+    # Index of the first element that is smaller than
+    # the element to its right.
+    index = -1
+    # Loop from right to left
+    for i in range(n - 1, 0, -1):
+        if nums[i] > nums[i - 1]:
+            index = i - 1
+            break
+    # Base condition
+    if index == -1:
+        reverse(nums, 0, n - 1)
+        return
+    j = n - 1
+    # Again swap from right to left to find first element
+    # that is greater than the above find element
+    for i in range(n - 1, index, -1):
+        if nums[i] > nums[index]:
+            j = i
+            break
+    # Swap the elements
+    nums[index], nums[j] = nums[j], nums[index]
+    # Reverse the elements from index + 1 to the nums.length
+    reverse(nums, index + 1, n - 1)
