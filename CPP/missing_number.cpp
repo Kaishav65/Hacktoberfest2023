7838256fa9ae87a69eabfcc589d8bac9f6ed1695
Naveen Dahiya <75837395+Naveenmico@users.noreply.github.com>
2023-10-02 22:54:24 +0530

    Create missing_number.cpp
    Given an array of size N-1 such that it only contains distinct integers in the range of 1 to N. Find the missing element.

---
+++ CPP/missing_number.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution{
+  public:
+    int MissingNumber(vector<int>& array, int n) {
+        int total = (n + 1) * (n) / 2;
+        for (int i = 0; i < n-1; i++)
+            total -= array[i];
+        return total;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> array(n - 1);
+        for (int i = 0; i < n - 1; ++i) cin >> array[i];
+        Solution obj;
+        cout << obj.MissingNumber(array, n) << "\n";
+    }
+    return 0;
+}
