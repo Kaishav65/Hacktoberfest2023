77c9b13c99f8a57a57d739c6f932fcb9a9b01f77
Krzysztof Majerczyk-Marchewka <skanerooo5@gmail.com>
2023-10-02 19:08:57 +0200

    Fixed bugs and optimized Friday.py

--- Python/Friday.py
+++ Python/Friday.py
@@ -5,10 +5,6 @@ import webbrowser       # For opening web pages
 import speech_recognition as sr  # For speech recognition
 import time             # For time-related operations
 
-# Get the current timestamp
-timestamp = time.strftime('%H hours %M minutes %S seconds')
-# Get the current hour
-hour = int(time.strftime('%H'))
 
 # Choose the voice for the virtual assistant
 your_voice_index = 1
@@ -36,46 +32,49 @@ if __name__ == '__main__':
     
     while True:
         print("Listening.....")
-        query = takeCommand()
+        query = takeCommand().lower()
         
         # Define a list of websites to open
-        sites = [["Youtube", "https://www.youtube.com"],
-                 ["Instagram", "https://www.instagram.com"],
-                 ["Github", "https://www.github.com"],
-                 ["Whatsapp", "https://www.whatsapp.com"],
+        sites = [["youtube", "https://www.youtube.com"],
+                 ["instagram", "https://www.instagram.com"],
+                 ["withub", "https://www.github.com"],
+                 ["whatsapp", "https://www.whatsapp.com"],
                  ["geeksforgeeks", "https://practice.geeksforgeeks.org/explore?page=1&sortBy=submissions&utm_source=geeksforgeeks&utm_medium=main_header&utm_campaign=practice_header"]]
         
         # Check if the user wants to open a website
         for site in sites:
-            if f"open {site[0]}".lower() in query.lower():
+            if f"open {site[0]}" in query:
                 speaker.speak(f"opening {site[0]} sir")
                 webbrowser.open(site[1])
                 
         # Check for time-based greetings
-        if query.lower() == f"good morning friday" or query.lower() == f"good afternoon Friday" or query.lower() == f"good evening Friday":
-            if hour >= 6 and hour <= 12:
+        greetings = ["good morning friday", "good afternoon friday", "good evening friday"]
+        if any([greeting in query for greeting in greetings]): # replaces with list comprehension to make it faster
+            # Get the current hour
+            hour = time.localtime().tm_hour # moved into if statement to make sure response is correct after longer usage
+            if hour > 6 and hour <= 12:
                 speaker.speak(f"good morning sir, how can I help you")
-            elif hour >= 12 and hour <= 17:
+            elif hour > 12 and hour <= 17:
                 speaker.speak(f"good afternoon sir, how can I help you")
-            elif hour >= 17 and hour <= 20:
-                speaker.speak(f"good evening sir, how can I help you")
-            elif hour >= 20 and hour <= 6:
-                speaker.speak(f"good evening sir, how can I help you")
+            elif hour > 17 or hour <= 6:
+                speaker.speak(f"good evening sir, how can I help you") # removed unnecessary impossible case
             
         # Check for a "good night" command
-        if "good night Friday" in query.lower():
+        if "good night friday" in query:
             speaker.speak(f"good night sir, have a nice day ahead")
             break
         
         # Check for a "what is the time" command
-        if "what is the time" in query.lower():
+        if "what is the time" in query:
+            # Get the current timestamp
+            timestamp = time.strftime('%H hours %M minutes %S seconds') # moved into if statement to make sure response is correct
             speaker.speak(f"time is: {timestamp}")
         
         # Check for a greeting
-        if "hello Friday" in query.lower():
+        if "hello friday" in query:
             speaker.speak(f"I'm fine sir, how are you")
         
         # Check for a command to stop the program
-        if "friday stop" in query.lower():
+        if "friday stop" in query:
             speaker.speak(f"Sure sir")
             break
