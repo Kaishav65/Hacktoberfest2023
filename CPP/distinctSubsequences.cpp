dab5794197fb682a720022bfb52e44c5f4fb38d2
Kaishav45 <115387293+Kaishav45@users.noreply.github.com>
2023-10-03 20:05:34 +0530

    Add files via upload

---
+++ CPP/distinctSubsequences.cpp
@@ -0,0 +1,16 @@
+class Solution{
+  public:	
+	int distinctSubsequences(string s)
+	{
+	     int n=s.size();
+	    long long mod=1e9+7;
+	    long long arr[26]={0},pre=1,cur=1;
+	    for(int i=0;i<n;i++){
+	        cur=(pre*2)%mod;
+	        cur = (cur - arr[s[i] - 'a'] + mod) % mod;
+	        arr[s[i]-'a']=pre;
+	        pre=cur;
+	    }
+	    return cur;
+	}
+};
