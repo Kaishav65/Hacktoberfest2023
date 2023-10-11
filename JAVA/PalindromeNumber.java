073a5f00df691d52ce50a6fbb94ad38f69b96578
Aliza Dixit <alizadixit6@gmail.com>
2023-10-11 13:25:40 +0530

    Update PalindromeNumber.java
    done with palindrome number

--- JAVA/PalindromeNumber.java
+++ JAVA/PalindromeNumber.java
@@ -24,3 +24,33 @@ public class PalindromeNumber
             System.out.println(num+" is not a Palindrome number");
     }
 }
+
+
+class hacktoberfest2023 { 
+    // Iterative function to 
+    // reverse the digits of number 
+    static int reversNumber(int n) 
+    { 
+        int reversed_n = 0; 
+        while (n > 0) { 
+            reversed_n = reversed_n * 10 + n % 10; 
+            n = n / 10; 
+        } 
+        return reversed_n; 
+    } 
+  
+    // Main function 
+    public static void main(String[] args) 
+    { 
+        int n = 123464321; 
+        int reverseN = reversNumber(n); 
+        System.out.println("Reverse of n = " + reverseN); 
+  
+        // Checking if n is same 
+        // as reverse of n 
+        if (n == reverseN) 
+            System.out.println("Palindrome = Yes"); 
+        else
+            System.out.println("Palindrome = No"); 
+    } 
+}
